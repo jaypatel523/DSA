333b5dff2864a811feda6760eb940d51c6c25f09
jaypatel523 <97498433+jaypatel523@users.noreply.github.com>
2022-09-06 09:31:37 +0530

    Add another solution

--- Codeforces/Div-3/C_Frog_Jumps.cpp
+++ Codeforces/Div-3/C_Frog_Jumps.cpp
@@ -14,6 +14,9 @@ int main()
 		int size = s.size();
 
 		
+		// my solution 
+		// TC O(n) and SC O(1)
+		
 		int i=-1;
 		int j=0;
 
@@ -34,6 +37,39 @@ int main()
 
 		ans = max(ans, j - i);
 		cout << ans << endl;
+		
+		
+		
+		
+		
+		
+		
+		// another approach 
+		// Tc -> O(n) but we will use array to store the indices of R
+
+
+		vector<int> r;
+		r.push_back(0);
+		for(int i=0; i<size; i++)
+		{
+			if(s[i] == 'R')
+			{
+				r.push_back(i+1);
+			}
+		}
+		r.push_back(size + 1);
+
+
+		int maxDif = 0;
+		for(int i=1; i<r.size(); i++)
+		{
+			maxDif = max(maxDif, r[i] - r[i-1]);
+		}
+
+		cout << maxDif;
+
+		
+		cout << endl;
 
 
 	}
