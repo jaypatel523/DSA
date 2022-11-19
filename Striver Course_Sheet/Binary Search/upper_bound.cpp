a029ea8d358b53e00e6c2c9490d1f1719f23115b
jaypatel523 <97498433+jaypatel523@users.noreply.github.com>
2022-11-19 19:04:07 +0530

    Create upper_bound.cpp
    https://practice.geeksforgeeks.org/problems/ceil-the-floor2802/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=ceil-the-floor

---
+++ Striver Course_Sheet/Binary Search/upper_bound.cpp
@@ -0,0 +1,27 @@
+int ceil(int arr[], int n, int x)
+{
+    int low = 0;
+    int high = n - 1;
+    int ans = -1;
+    
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        
+        if(arr[mid] == x)
+        {
+            return arr[mid];
+        }
+        else if(arr[mid] > x)
+        {   
+            ans = arr[mid];
+            high = mid - 1;
+        }
+        else 
+        {
+            low = mid + 1;
+        }
+    }
+    
+    return ans;
+}
