e28588ad19f9b7aacd3da901652cf9b5451ceb7c
jaypatel523 <97498433+jaypatel523@users.noreply.github.com>
2022-11-19 18:59:35 +0530

    Update lowe_bound.cpp
    https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%3BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array

--- Striver Course_Sheet/Binary Search/lowe_bound.cpp
+++ Striver Course_Sheet/Binary Search/lowe_bound.cpp
@@ -1,106 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-
-
-ll firstOccurence(vector<ll> & arr, ll target)
-{
-	ll low = 0;
-	ll high = arr.size() - 1;
-	ll ans = -1;
-
-	while(low <= high)
-	{
-		ll mid = low + (high - low) / 2;
-
-		if(arr[mid] == target)
-		{
-			ans = mid;
-			high = mid - 1;
-		}
-		else if(arr[mid] > target)
-		{
-			high = mid - 1;
-		}
-		else 
-		{
-			low = mid + 1;
-		}
-	}
-	return ans;
-}
-
-
-
-
-ll lastOccurence(vector<ll> & arr, ll target)
-{
-	ll low = 0;
-	ll high = arr.size() - 1;
-	ll ans = -1;
-
-	while(low <= high)
-	{
-		ll mid = low + (high - low) / 2;
-
-		if(arr[mid] == target)
-		{
-			ans = mid;
-			low = mid + 1;
-		}
-		else if(arr[mid] < target)
-		{
-			low = mid + 1;
-		}
-		else 
-		{
-			high = mid - 1;
-		}
-	}
-	return ans;
-}
-
-
-
-void solve(vector<ll> & arr, ll target)
-{
-	ll n = arr.size();
-
-	ll first = firstOccurence(arr, target);
-	ll last = lastOccurence(arr, target);
-
-	cout << "First : " << first << endl;
-	cout << "Last : " << last << endl;
-}
-
-
-
-int main()
-{
-	int t;
-	cin >> t;
-
-	while(t--)
-	{
-		ll n;
-		cin >> n;
-
-
-		vector<ll> arr;
-		for(int i=0; i<n; i++)
-		{
-			ll x;
-			cin >> x;
-			arr.push_back(x);
-		}	
-
-		ll target;
-		cin >> target;
-
-		solve(arr, target);
-
-
-	}
-
-	return 0;
-}
+int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        
+        while(low <= high)
+        {
+            long long mid = low + (high - low) / 2;
+            if(v[mid] == x)
+            {
+                ans = mid;
+                break;
+            }
+            else if(v[mid] > x)
+            {
+                high = mid - 1;
+            }
+            else 
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+        
+    }
