909819e754a940afe3dfdf420f4f4ff211d5d54e
jaypatel523 <97498433+jaypatel523@users.noreply.github.com>
2022-11-25 20:03:09 +0530

    Medium
    https://leetcode.com/problems/find-peak-element/#:~:text=Find%20Peak%20Element%20%2D%20LeetCode&text=A%20peak%20element%20is%20an,to%20any%20of%20the%20peaks.

--- Striver Course_Sheet/Binary Search/peak_element.cpp
+++ Striver Course_Sheet/Binary Search/peak_element.cpp
@@ -28,7 +28,7 @@ Explanation: Your function can return either index number 1 where the peak eleme
 
 
 Constraints:
-
+  
 1 <= nums.length <= 1000
 -231 <= nums[i] <= 231 - 1
 nums[i] != nums[i + 1] for all valid i.
