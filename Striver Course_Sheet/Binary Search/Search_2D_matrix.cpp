29f0aac7f9ddd27ab693decc84fd4bf595ef3d1f
jaypatel523 <97498433+jaypatel523@users.noreply.github.com>
2022-11-25 20:02:15 +0530

    Medium - 2D
    https://leetcode.com/problems/search-a-2d-matrix/

--- Striver Course_Sheet/Binary Search/Search_2D_matrix.cpp
+++ Striver Course_Sheet/Binary Search/Search_2D_matrix.cpp
@@ -11,7 +11,7 @@ The first integer of each row is greater than the last integer of the previous r
 
 Example 1:
 
-Input: matrix = [[1 , 3, 5, 7],
+Input: matrix = [[1 , 3, 5, 7],  
                  [10,11,16,20],
                  [23,30,34,60]]
 target = 3
